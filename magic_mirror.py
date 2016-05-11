008495efa14598d484cc420386797dbc7be08753
Drew.Russell@outlook.com <Drew.Russell@outlook.com>
2016-05-11 14:34:32 -0500

    switch to the forecastio API

--- magic_mirror.py
+++ magic_mirror.py
@@ -3,36 +3,19 @@
 import datetime
 import calendar
 import time
-import pyowm
+import forecastio
 from flask import Flask, render_template
 
 
-CITY = 'Saint Louis, US'
+# API Key for ForecastIO
+api_key = ""
 
-WEATHER = pyowm.OWM('a59738017ea5ee759dcf34f47f5f1d8c')
+# Latitude and Longitude for desired location
+lat = ""
+lng = ""
 
-LOCATION = WEATHER.weather_at_place(CITY)
-
-CURRENT_WEATHER = LOCATION.get_weather()
-
-
-def current_temperature():
-    """ Get the current temperature. """
-
-    temp_stats = CURRENT_WEATHER.get_temperature('fahrenheit')
-
-    for key, value in temp_stats.iteritems():
-        if key == "temp":
-            temp = round(value)
-
-    return str(int(temp))
-
-
-def current_weather_conditions():
-    """ Get the current status of the weathe (ex. Clear Sky) """
-    condition_status = CURRENT_WEATHER.get_detailed_status()
-
-    return condition_status
+WEATHER = forecastio.load_forecast(api_key, lat, lng,)
+HOURLY_WEATHER = WEATHER.hourly()
 
 
 def date_time():
@@ -50,54 +33,44 @@ def date_time():
     return current_date
 
 
+def current_weather_temp():
+    """ Get the current temperature. """
+
+    for data in HOURLY_WEATHER.data[:1]:
+        temperature = int(round(data.temperature))
+        return temperature
+
+
+def current_weather_summary():
+    """ Get the current weather summary. """
+
+    for data in HOURLY_WEATHER.data[:1]:
+        summary = data.summary
+        return summary
+
+
+def current_weather_icon():
+    """ Get the current weather icon. """
+
+    for data in HOURLY_WEATHER.data[:1]:
+        icon = data.icon.replace(" ", "-")
+        wi = "wi-forecast-io-" + icon
+        return wi
+
+
 def current_time():
     """ Get the current time. Used in weather_icon()"""
     current_time.now = time.time()
     return current_time.now
-
-
-def sunrise():
-    """ Get todays sunrise time. Used in weather_icon()"""
-    sunrise.time = CURRENT_WEATHER.get_sunrise_time()
-    return sunrise.time
-
-
-def sunset():
-    """ Get todays sunset time. Used in weather_icon()"""
-    sunset.time = CURRENT_WEATHER.get_sunset_time()
-    return sunset.time
-
-
-def weather_icon():
-    """ Get the OWN weather code and the format correctly for the CSS weather
-    icons"""
-
-    # Get the weather code from the OpenWeatherAPI
-    weather_code = CURRENT_WEATHER.get_weather_code()
-
-    # Replace any spaces with a - for proper HTML formatting
-    icon_label = "owf owf-" + str(weather_code)
-
-    # Determine if it is night or day and get the proper prefix
-
-    if sunrise() <= current_time() <= sunset():
-        label_suffix = "-d"
-    else:
-        label_suffix = "-n"
-
-    icon_label = (icon_label + label_suffix)
-
-    return icon_label
-
 
 APP = Flask(__name__)
 
 
 @APP.route('/')
 def index():
-    """ Create the HTHML Page """
+    """ Create the HTML Page """
 
-    return render_template('index.html', date=date_time(), temp=current_temperature(), conditions=current_weather_conditions(), weather=weather_icon())
+    return render_template('index.html', date=date_time(), temp=current_weather_temp(), conditions=current_weather_summary(), wi=current_weather_icon())
 
 if __name__ == '__main__':
-    APP.run()
+    APP.run(debug=True)
